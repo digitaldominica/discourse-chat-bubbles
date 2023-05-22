a2d05055820473ed69b8e68efc11b7668219f3bd
chapoi <charlie@discourse.org>
2023-05-22 23:11:03 +0900

    adding settings

--- javascripts/discourse/api-initializers/chat-bubbles.js
+++ javascripts/discourse/api-initializers/chat-bubbles.js
@@ -1,3 +1,43 @@
 import { apiInitializer } from "discourse/lib/api";
 
-export default apiInitializer("0.11.1", (api) => {});
+export default apiInitializer("0.11.1", (api) => {
+  function setupComponent() {
+    const light_bubble_color_1 = settings.light_bubble_color_1
+      ? settings.light_bubble_color_1
+      : "var(--tertiary-low)";
+
+    const light_bubble_color_2 = settings.light_bubble_color_2
+      ? settings.light_bubble_color_2
+      : "var(--secondary)";
+
+    const background_color = settings.background_color
+      ? settings.background_color
+      : "var(--secondary-very-high)";
+
+    const show_background_pattern = settings.show_background_pattern
+      ? "url($chat-background)"
+      : "none";
+
+    document.documentElement.style.setProperty(
+      "--light-bubble-color-1",
+      light_bubble_color_1
+    );
+
+    document.documentElement.style.setProperty(
+      "--light-bubble-color-2",
+      light_bubble_color_2
+    );
+
+    document.documentElement.style.setProperty(
+      "--background-color",
+      background_color
+    );
+
+    document.documentElement.style.setProperty(
+      "--bg-pattern",
+      show_background_pattern
+    );
+  }
+
+  setupComponent();
+});
