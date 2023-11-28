ae03e9024b454057364c1229c69b7fd71187f030
chapoi <101828855+chapoi@users.noreply.github.com>
2023-11-28 10:29:45 +0200

    Dark mode > Bubbles

--- javascripts/discourse/api-initializers/chat-bubbles.js
+++ javascripts/discourse/api-initializers/chat-bubbles.js
@@ -2,13 +2,21 @@ import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("0.11.1", (api) => {
   function setupComponent() {
-    const light_bubble_color_1 = settings.light_bubble_color_1
-      ? settings.light_bubble_color_1
-      : "var(--tertiary-low)";
+    // const light_bubble_color_1 = settings.light_bubble_color_1
+    //   ? settings.light_bubble_color_1
+    //   : "var(--tertiary-low)";
 
-    const light_bubble_color_2 = settings.light_bubble_color_2
-      ? settings.light_bubble_color_2
-      : "var(--secondary)";
+    // const light_bubble_color_2 = settings.light_bubble_color_2
+    //   ? settings.light_bubble_color_2
+    //   : "var(--secondary)";
+
+    // const dark_bubble_color_1 = settings.dark_bubble_color_1
+    //   ? settings.dark_bubble_color_1
+    //   : "var(--tertiary-low)";
+
+    // const dark_bubble_color_2 = settings.dark_bubble_color_2
+    //   ? settings.dark_bubble_color_2
+    //   : "var(--secondary)";
 
     const background_color = settings.background_color
       ? settings.background_color
@@ -18,15 +26,15 @@ export default apiInitializer("0.11.1", (api) => {
       ? "url($chat-background)"
       : "none";
 
-    document.documentElement.style.setProperty(
-      "--light-bubble-color-1",
-      light_bubble_color_1
-    );
+    // document.documentElement.style.setProperty(
+    //   "--light-bubble-color-1",
+    //   light_bubble_color_1
+    // );
 
-    document.documentElement.style.setProperty(
-      "--light-bubble-color-2",
-      light_bubble_color_2
-    );
+    // document.documentElement.style.setProperty(
+    //   "--light-bubble-color-2",
+    //   light_bubble_color_2
+    // );
 
     document.documentElement.style.setProperty(
       "--background-color",
