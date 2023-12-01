bc187f10dce40937f0c883e1220b34a67b7a55df
chapoi <101828855+chapoi@users.noreply.github.com>
2023-12-01 14:06:10 +0200

    cleanup

--- javascripts/discourse/api-initializers/chat-bubbles.js
+++ javascripts/discourse/api-initializers/chat-bubbles.js
@@ -5,46 +5,37 @@ export default apiInitializer("0.11.1", (api) => {
     // const light_bubble_color_1 = settings.light_bubble_color_1
     //   ? settings.light_bubble_color_1
     //   : "var(--tertiary-low)";
-
     // const light_bubble_color_2 = settings.light_bubble_color_2
     //   ? settings.light_bubble_color_2
     //   : "var(--secondary)";
-
     // const dark_bubble_color_1 = settings.dark_bubble_color_1
     //   ? settings.dark_bubble_color_1
     //   : "var(--tertiary-low)";
-
     // const dark_bubble_color_2 = settings.dark_bubble_color_2
     //   ? settings.dark_bubble_color_2
     //   : "var(--secondary)";
-
-    const background_color = settings.background_color
-      ? settings.background_color
-      : "var(--secondary-very-high)";
-
-    const show_background_pattern = settings.show_background_pattern
-      ? "url($chat-background)"
-      : "none";
-
+    // const background_color = settings.background_color
+    //   ? settings.background_color
+    //   : "var(--secondary-very-high)";
+    // const show_background_pattern = settings.show_background_pattern
+    //   ? "url($chat-background)"
+    //   : "none";
     // document.documentElement.style.setProperty(
     //   "--light-bubble-color-1",
     //   light_bubble_color_1
     // );
-
     // document.documentElement.style.setProperty(
     //   "--light-bubble-color-2",
     //   light_bubble_color_2
     // );
-
-    document.documentElement.style.setProperty(
-      "--background-color",
-      background_color
-    );
-
-    document.documentElement.style.setProperty(
-      "--bg-pattern",
-      show_background_pattern
-    );
+    // document.documentElement.style.setProperty(
+    //   "--background-color",
+    //   background_color
+    // );
+    // document.documentElement.style.setProperty(
+    //   "--bg-pattern",
+    //   show_background_pattern
+    // );
   }
 
   setupComponent();
